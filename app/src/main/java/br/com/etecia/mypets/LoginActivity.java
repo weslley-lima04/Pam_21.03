98093be0e259316dae6532d72339ef73b5f398ed
weslley-lima04 <weslley.lima24@etec.sp.gov.br>
2022-04-25 20:23:02 -0300

    criamos o método sair para encerrar o programa

--- app/src/main/java/br/com/etecia/mypets/LoginActivity.java
+++ app/src/main/java/br/com/etecia/mypets/LoginActivity.java
@@ -3,12 +3,40 @@ package br.com.etecia.mypets;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+    //Declaração de componentes
+    EditText edt_user, edt_pass;
+    Button btn_entrar, btn_sair;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
+
+        edt_user = findViewById(R.id.edt_user);
+        edt_pass = findViewById(R.id.edt_pass);
+        btn_entrar = findViewById(R.id.btn_entrar);
+        btn_sair = findViewById(R.id.btn_sair);
+
+        btn_sair.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                finish();
+            }
+        });
+
+
+    }
+
+    public void acessarJanela(View view)
+    {
+
     }
 }
