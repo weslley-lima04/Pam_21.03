235a6626364b779f06d8e160cbb7bf149e8e7e31
weslley-lima04 <weslley.lima24@etec.sp.gov.br>
2022-05-02 19:10:23 -0300

    criamos limpar tela e entrar

--- app/src/main/java/br/com/etecia/mypets/LoginActivity.java
+++ app/src/main/java/br/com/etecia/mypets/LoginActivity.java
@@ -2,10 +2,12 @@ package br.com.etecia.mypets;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
     //Declaração de componentes
@@ -32,8 +34,42 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        btn_entrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String nome = edt_user.getText().toString();
+                String email = edt_pass.getText().toString();
+
+                if (nome.equals("etecia") && email.equals("etecia"))
+                {
+
+                    //abrindo outra janela
+                    startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                }
+                else
+                {
+                    Toast.makeText(getApplicationContext(),
+                            "Usuário ou senha inválidos!!!",
+                            Toast.LENGTH_SHORT).show();
+                    //chamando método limpar tela
+                    limparTela();
+                }
+            }
+        });
+
 
     }
+
+    //criando o método para limpar a tela
+    public void limparTela()
+    {
+        edt_user.setText("");
+        edt_pass.setText("");
+        edt_user.requestFocus();
+    }
+
+
 
     public void acessarJanela(View view)
     {
