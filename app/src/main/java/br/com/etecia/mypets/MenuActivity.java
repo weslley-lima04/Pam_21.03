52cf17cddda8c872b94030e8c3045f9ce325f869
weslley-lima04 <weslley.lima24@etec.sp.gov.br>
2022-05-04 19:33:09 -0300

    criamos método para seleção de menu

--- app/src/main/java/br/com/etecia/mypets/MenuActivity.java
+++ app/src/main/java/br/com/etecia/mypets/MenuActivity.java
@@ -1,8 +1,13 @@
 package br.com.etecia.mypets;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -10,5 +15,28 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_layout);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_pets, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.icCadastrar:
+                Toast.makeText(
+                        getApplicationContext(),
+                        "Cadastrado com sucesso",
+                        Toast.LENGTH_LONG
+                ).show();
+        }
+
+
+        return super.onOptionsItemSelected(item);
     }
 }
