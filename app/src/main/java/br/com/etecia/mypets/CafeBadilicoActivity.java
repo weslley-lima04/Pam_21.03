12bfef1e51583a4add5ac4dd56c631b83b853ba6
weslley-lima04 <weslley.lima24@etec.sp.gov.br>
2022-05-11 19:44:28 -0300

    Criamos parte do card

---
+++ app/src/main/java/br/com/etecia/mypets/CafeBadilicoActivity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.mypets;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CafeBadilicoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cafe_badilico_layout);
+    }
+}
