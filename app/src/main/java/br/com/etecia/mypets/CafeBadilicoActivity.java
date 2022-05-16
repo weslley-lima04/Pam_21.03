d81e6d97e09a70a8e3faa0559ba6a6c2a0e96188
weslley-lima04 <weslley.lima24@etec.sp.gov.br>
2022-05-16 19:25:20 -0300

    criamos funções para os botões do card

--- app/src/main/java/br/com/etecia/mypets/CafeBadilicoActivity.java
+++ app/src/main/java/br/com/etecia/mypets/CafeBadilicoActivity.java
@@ -1,14 +1,74 @@
 package br.com.etecia.mypets;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CafeBadilicoActivity extends AppCompatActivity {
+    CardView idCardView;
+    Button btnCincoeMeia;
+    Button btnSeteeMeia;
+    Button btnOitohoras;
+    Button btnNovehoras;
+    Button btnReserve;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cafe_badilico_layout);
+        //Cardview
+        idCardView = findViewById(R.id.idCardView);
+        idCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        //BtnCincoeMeia
+        btnCincoeMeia = findViewById(R.id.btnCincoeMeia);
+        btnCincoeMeia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+        //BtnSeteeMeia
+        btnSeteeMeia = findViewById(R.id.btnSeteeMeia);
+        btnSeteeMeia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        //BtnOitohoras
+        btnOitohoras = findViewById(R.id.btnOitohoras);
+        btnOitohoras.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        //BtnNovehoras
+        btnNovehoras = findViewById(R.id.btnNovehoras);
+        btnNovehoras.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        //BtnReserve
+        btnReserve = findViewById(R.id.btnReserve);
+        btnReserve.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
